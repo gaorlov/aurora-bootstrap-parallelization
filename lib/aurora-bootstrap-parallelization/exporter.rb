ec855f4c558e7dd55caf794d84b9d1a018f26fc2
Greg Orlov <gaorlov@gmail.com>
2019-09-11 22:37:15 -0700

    fleshing out most of the exporter class; building job manifest template

--- lib/aurora-bootstrap-parallelization/exporter.rb
+++ lib/aurora-bootstrap-parallelization/exporter.rb
@@ -5,5 +5,24 @@ module AuroraBootstrapParallelization
     end
 
     def deploy
+      # make file named tmp/#{self.name}-job.yml
+      # run kubectl create
+    end
+
+    def manifest
+      @manifest ||= YAML.load(ERB.new(File.read("../../templates/job.yml")).result( self ))
+    end
+
+    def name
+      @name ||= "#{config[:host]}-exporter"
+    end
+
+    def env
+      @env ||= config[:env]
+    end
+
+    def version
+      @version ||= config[:version]
+    end
   end
 end
