14f27dc8325edac460587e84f79faa0b4f9a4e79
YinfangZ <yinfang.zhuang@outreach.io>
2021-05-26 12:56:51 -0700

    Create a separate job template file for cronjob

--- lib/aurora_bootstrap_parallelization/exporter.rb
+++ lib/aurora_bootstrap_parallelization/exporter.rb
@@ -3,10 +3,11 @@ require 'erb'
 
 module AuroraBootstrapParallelization
   class Exporter
-    def initialize( config )
+    def initialize( config, is_cron_job )
       @name    = config[ "name" ]
       @env     = config[ "env" ]
       @version = config[ "version" ]
+      @is_cron_job = to_b( is_cron_job )
     end
 
     def deploy
@@ -16,19 +17,34 @@ module AuroraBootstrapParallelization
     end
 
     def manifest
-      @manifest ||= begin
+      if @is_cron_job
+        path = File.expand_path( "../../../templates/cronjob.yml", __FILE__ )
+        @manifest ||= begin
+          erb = ERB.new( File.read( path ) ).result( binding )
+          YAML.load( erb ).tap do | manifest |
+            manifest["spec"]["jobTemplate"][ "spec" ][ "template" ][ "spec" ][ "containers" ][0][ "env" ] = @env
+          end
+        end
+      else
         path = File.expand_path( "../../../templates/job.yml", __FILE__ )
-        erb = ERB.new( File.read( path ) ).result( binding )
-        YAML.load( erb ).tap do | manifest |
-          manifest["spec"]["jobTemplate"][ "spec" ][ "template" ][ "spec" ][ "containers" ][0][ "env" ] = @env
+        @manifest ||= begin
+          erb = ERB.new( File.read( path ) ).result( binding )
+          YAML.load( erb ).tap do | manifest |
+            manifest[ "spec" ][ "template" ][ "spec" ][ "containers" ][0][ "env" ] = @env
+          end
         end
       end
     end
 
     def file_name
-      @file_name ||= "#{@name}-job.yml"
+      if @is_cron_job
+        @file_name ||= "#{@name}-cronjob.yml"
+      else
+        @file_name ||= "#{@name}-job.yml"
+      end
+      @file_name
     end
-      
+ 
     def kubectl_deploy
       spawn "kubectl -n aurora-bootstrap create -f /tmp/#{file_name}"
     end
@@ -37,6 +53,14 @@ module AuroraBootstrapParallelization
       file = File.open("/tmp/#{file_name}", 'w')
       file.write manifest.to_yaml
       file.close
+    end
+
+    def to_b(string)
+      case string
+      when /^(TRUE|true|t|yes|y|1)$/i then true
+      when /^(FALSE|false|f|no|n|0)$/i then false
+      else raise "Cannot convert to boolean: #{string}"
+      end
     end
   end
 end
--- lib/aurora_bootstrap_parallelization/runner.rb
+++ lib/aurora_bootstrap_parallelization/runner.rb
@@ -2,6 +2,7 @@ module AuroraBootstrapParallelization
   class Runner
     def initialize( opts )
       @config = YAML.load_file( opts[ :config ] )[ "exporters" ]
+      @is_cron_job = opts[:cronjob]
     end
 
     def run!
@@ -12,7 +13,7 @@ module AuroraBootstrapParallelization
 
     def exporters
       @config.map do | env_config |
-        Exporter.new( env_config )
+        Exporter.new( env_config, @is_cron_job )
       end
     end
   end
