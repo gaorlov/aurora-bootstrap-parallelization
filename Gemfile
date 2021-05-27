54a24773eeefd0dadc3e873e6cd3e6ea10b18bec
YinfangZ <yinfang.zhuang@outreach.io>
2021-05-27 14:56:26 -0700

    Address one comment

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source 'https://rubygems.org'
 gem 'slop'
 gem 'rake'
 gem 'zeitwerk'
-gem 'ruby_dig'
 
 group :development, :test do
   gem 'minitest'
--- lib/aurora_bootstrap_parallelization/job_exporter.rb
+++ lib/aurora_bootstrap_parallelization/job_exporter.rb
@@ -1,11 +1,10 @@
 require 'yaml'
 require 'erb'
-require 'ruby_dig'
 
 module AuroraBootstrapParallelization
   # for one time run of aurora-bootstrap script
   class JobExporter < Exporter
-    
+
     def initialize( config )
       super( config )
       @type = 'job'
